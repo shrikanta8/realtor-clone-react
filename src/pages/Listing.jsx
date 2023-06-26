3b5efd7b1b11d080f112bc57e0772017920efff2
Shrikanta <kalantrishriji8@gmail.com>
2023-06-26 14:43:45 +0530

    made changes in swiper

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,55 +1,59 @@
-import { doc, getDoc } from "firebase/firestore"
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { doc, getDoc } from "firebase/firestore";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Spinner from "../components/Spinner";
-import { db } from "../firebase"
-// swiper
-import {Swiper, SwiperSlide} from "swiper/react"
-import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination, } from 'swiper';
-import 'swiper/css/bundle';
+import { db } from "../firebase";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  EffectFade,
+  Autoplay,
+  Navigation,
+  Pagination,
+} from "swiper";
+import "swiper/css/bundle";
 
 export default function Listing() {
-    const params = useParams()
-    const [listing, setListing] = useState(null)
-    const [loading, setLoading] = useState(true)
-
-    SwiperCore.use([Autoplay, Navigation, Pagination])
-    useEffect(() => {
-      async function fetchListing(){
-        const docRef = doc(db, "listings", params.listingId)
-        const docSnap = await getDoc(docRef)
-        if(docSnap.exists()){
-            setListing(docSnap.data())
-            setLoading(false)
-            console.log(listing)
-        }
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  SwiperCore.use([Autoplay, Navigation, Pagination]);
+  useEffect(() => {
+    async function fetchListing() {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        setLoading(false);
       }
-      fetchListing()
-    }, [params.listingId])
-    console.log(listing)
-
-    if(loading){
-        return <Spinner/>
     }
-
+    fetchListing();
+  }, [params.listingId]);
+  if (loading) {
+    return <Spinner />;
+  }
   return (
     <main>
-      <Swiper 
-        slidesPerView={1} navigation pagination={{type: "progressbar"}} effect="fade" modules={[EffectFade]} autoplay={{ delay: 3000 }} 
+      <Swiper
+        slidesPerView={1}
+        navigation
+        pagination={{ type: "progressbar" }}
+        effect="fade"
+        modules={[EffectFade]}
+        autoplay={{ delay: 3000 }}
       >
-          {listing.imgUrls.map((url,index) => (
-              <SwiperSlide key={index}>
-                  <div 
-                      className=" relative w-full overflow-hidden h-[300px]"
-                      style={{
-                        background: `url(${listing.imgUrls[index]}) center no-repeat` , backgroundSize: "cover",
-                      }} 
-                  >
-                  </div>
-              </SwiperSlide>
-          ))}
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="relative w-full overflow-hidden h-[300px]"
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: "cover",
+              }}
+            ></div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </main>
-  )
-
+  );
 }
